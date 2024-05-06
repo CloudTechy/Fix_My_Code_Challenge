7b44bd60ced65b64786f12fb6f3494eeb1d77737
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-05-06 04:41:09 -0700

    done o

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -3,13 +3,12 @@
 
 
 class square():
-    """ Represents a Square """    
-    
+    """ Represents a Square """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ class constructor """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -19,14 +18,14 @@ class square():
 
     def PermiterOfMySquare(self):
         """ Perimeter of the square """
-        return ((self.width  + self.height) * 2)
+        return ((self.width + self.height) * 2)
 
     def __str__(self):
         """String implementation"""
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
